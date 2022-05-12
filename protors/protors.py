8af0b489ec80cfb64f03c6ea73c42a838d585fd3
quangdzuytran <quangduy0412@gmail.com>
2022-05-12 13:30:09 +0700

    Add properties getter and setter

--- protors/protors.py
+++ protors/protors.py
@@ -31,20 +31,56 @@ class ProtoRS(nn.Module):
                                         args.W1,
                                         args.H1,
                                         self.epsilon)
-        # RRL
+        # MLLP
         n_discrete_features = 0
         n_continuous_features = self.num_prototypes
         self.rs_dim_list = [(n_discrete_features, n_continuous_features)] + \
                             list(map(int, args.structure.split('@'))) + \
                             [self.num_classes]
-        self.rrl = MLLP(dim_list=self.rs_dim_list, 
+        self.mllp = MLLP(dim_list=self.rs_dim_list, 
                         use_not=use_not, 
                         left=left, 
                         right=right, 
                         estimated_grad=estimated_grad)
     
+    @property
+    def features_requires_grad(self) -> bool:
+        return any([param.requires_grad for param in self._net.parameters()])
+
+    @features_requires_grad.setter
+    def features_requires_grad(self, val: bool):
+        for param in self._net.parameters():
+            param.requires_grad = val
+
+    @property
+    def add_on_layers_requires_grad(self) -> bool:
+        return any([param.requires_grad for param in self._add_on.parameters()])
+
+    @add_on_layers_requires_grad.setter
+    def add_on_layers_requires_grad(self, val: bool):
+        for param in self._add_on.parameters():
+            param.requires_grad = val
+
+    @property
+    def prototypes_requires_grad(self) -> bool:
+        return self.prototype_layer.prototype_vectors.requires_grad
+
+    @prototypes_requires_grad.setter
+    def prototypes_requires_grad(self, val: bool):
+        self.prototype_layer.prototype_vectors.requires_grad = val
+
+    @property
+    def mllp_requires_grad(self) -> bool:
+        return self.mllp.layer_list[-1].requires_grad
+
+    @mllp_requires_grad.setter
+    def mllp_requires_grad(self, val: bool):
+        for layer in self.mllp.layer_list:
+            layer.requires_grad = val
+    
     def forward(self, 
-                xs: torch.Tensor):
+                xs: torch.Tensor
+                ) -> tuple:
         # Forward conv net
         features = self._net(xs)
         features = self._add_on(features)
@@ -52,5 +88,7 @@ class ProtoRS(nn.Module):
         # Compute simlarities
         similarities = self.prototype_layer(features, W, H)
         # Classify
-        out_cont, out_disc = self.rrl(similarities)
+        out_cont, out_disc = self.mllp(similarities)
         return out_cont, out_disc
+
+    
